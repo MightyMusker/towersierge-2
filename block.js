fff7d0807615e9845e339326944a5af1828a8aad
MightyMusker <74767936+MightyMusker@users.noreply.github.com>
2021-07-02 14:42:23 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,30 @@
+class Block{
+    constructor(x, y, width, height) {
+      this.visibility=255;
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+  //      var angle = this.body.angle;
+        var pos= this.body.position;
+        if(this.body.speed<3)
+        {
+          rectMode(CENTER);
+        rect(pos.x,pos.y,this.width, this.height);
+        }
+        else
+        {
+          World.remove(world,this.body);
+          push()
+          this.visibility=this.visibility-5
+          tint(255,this.visibility);
+          rect(pos.x,pos.y,this.width, this.height);
+          pop();
+        }
+      }
+}
